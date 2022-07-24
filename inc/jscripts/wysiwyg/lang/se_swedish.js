7876e6cf12620453f070550a46eb6863d7ffd2b7
Michael <michael.silverunit@gmail.com>
2022-07-24 02:13:00 +0200

    Add files via upload

---
+++ inc/jscripts/wysiwyg/lang/se_swedish.js
@@ -0,0 +1,155 @@
+(function($) {
+  $.extend($.summernote.lang, {
+    'se_swedish': {
+      font: {
+        bold: 'Fet',
+        italic: 'Kursiv',
+        underline: 'Understruken',
+        clear: 'Radera formatering',
+        height: 'Radavstånd',
+        name: 'Teckensnitt',
+        strikethrough: 'Genomstruken',
+        subscript: 'Subscript',
+        superscript: 'Superscript',
+        size: 'Teckenstorlek'
+      },
+      image: {
+        image: 'Bild',
+        insert: 'Infoga bild',
+        resizeFull: 'Full storlek',
+        resizeHalf: 'Halv storlek',
+        resizeQuarter: 'En fjärdedel i storlek',
+        floatLeft: 'Vänsterjusterad',
+        floatRight: 'Högerjusterad',
+        floatNone: 'Ingen justering',
+        shapeRounded: 'Shape: Rundad',
+        shapeCircle: 'Shape: Cirkel',
+        shapeThumbnail: 'Shape: Miniatyr',
+        shapeNone: 'Shape: Ingen',
+        dragImageHere: 'Dra en bild hit',
+        dropImage: 'Dra bild eller text',
+        selectFromFiles: 'Välj från filer',
+        maximumFileSize: 'Maximal filstorlek',
+        maximumFileSizeError: 'Maximal filstorlek överskriden.',
+        url: 'Länk till bild',
+        remove: 'Ta bort bild',
+        original: 'Original'
+      },
+      video: {
+        video: 'Filmklipp',
+        videoLink: 'Länk till filmklipp',
+        insert: 'Infoga filmklipp',
+        url: 'Länk till filmklipp',
+        providers: '(YouTube, Vimeo, Vine, Instagram, DailyMotion eller Youku)'
+      },
+      link: {
+        link: 'Länk',
+        insert: 'Infoga länk',
+        unlink: 'Ta bort länk',
+        edit: 'Redigera',
+        textToDisplay: 'Visningstext',
+        url: 'Till vilken URL ska denna länk peka?',
+        openInNewWindow: 'Öppna i ett nytt fönster'
+      },
+      table: {
+        table: 'Tabell',
+        addRowAbove: 'Lägg till rad ovan',
+        addRowBelow: 'Lägg till rad under',
+        addColLeft: 'Lägg till kolumn till vänster',
+        addColRight: 'Lägg till kolumn till höger',
+        delRow: 'Radera rad',
+        delCol: 'Radera kolumn',
+        delTable: 'Radera tabell'
+      },
+      hr: {
+        insert: 'Infoga horisontell linje'
+      },
+      style: {
+        style: 'Stil',
+        p: 'p',
+        blockquote: 'Citat',
+        pre: 'Kod',
+        h1: 'Rubrik 1',
+        h2: 'Rubrik 2',
+        h3: 'Rubrik 3',
+        h4: 'Rubrik 4',
+        h5: 'Rubrik 5',
+        h6: 'Rubrik 6'
+      },
+      lists: {
+        unordered: 'Punktlista',
+        ordered: 'Numrerad lista'
+      },
+      options: {
+        help: 'Hjälp',
+        fullscreen: 'Fullskärm',
+        codeview: 'HTML-visning'
+      },
+      paragraph: {
+        paragraph: 'Justera text',
+        outdent: 'Minska indrag',
+        indent: 'Öka indrag',
+        left: 'Vänsterjusterad',
+        center: 'Centrerad',
+        right: 'Högerjusterad',
+        justify: 'Justera text'
+      },
+      color: {
+        recent: 'Senast använda färg',
+        more: 'Fler färger',
+        background: 'Bakgrundsfärg',
+        foreground: 'Teckenfärg',
+        transparent: 'Genomskinlig',
+        setTransparent: 'Gör genomskinlig',
+        reset: 'Nollställ',
+        resetToDefault: 'Återställ till standard'
+      },
+      shortcut: {
+        shortcuts: 'Kortkommandon',
+        close: 'Stäng',
+        textFormatting: 'Textformatering',
+        action: 'Funktion',
+        paragraphFormatting: 'Avsnittsformatering',
+        documentStyle: 'Dokumentstil',
+        extraKeys: 'Extra keys'
+      },
+      help: {
+        'insertParagraph': 'Infoga stycke',
+        'undo': 'Ångrar det sista kommandot',
+        'redo': 'Gör om det senaste kommandot',
+        'tab': 'Tab',
+        'untab': 'Untab',
+        'bold': 'Ange en Fet stil',
+        'italic': 'Ange en Kursiv stil',
+        'underline': 'Ange en understruken stil',
+        'strikethrough': 'Ange en genomstruken stil',
+        'removeFormat': 'Radera format',
+        'justifyLeft': 'Ställ in vänsterjustering',
+        'justifyCenter': 'Ställ in mitten-centrering',
+        'justifyRight': 'Ställ in högerjustering',
+        'justifyFull': 'Ställ in fullständig justering',
+        'insertUnorderedList': 'Växla till oordnad lista',
+        'insertOrderedList': 'Växla till ordnad lista',
+        'outdent': 'Utdrag på nuvarande stycke',
+        'indent': 'Indrag på nuvarande stycke',
+        'formatPara': 'Ändra aktuellt block\'s format som ett stycke(P tag)',
+        'formatH1': 'Ändra aktuellt block\'s format till H1',
+        'formatH2': 'Ändra aktuellt block\'s format till H2',
+        'formatH3': 'Ändra aktuellt block\'s format till H3',
+        'formatH4': 'Ändra aktuellt block\'s format till H4',
+        'formatH5': 'Ändra aktuellt block\'s format till H5',
+        'formatH6': 'Ändra aktuellt block\'s format till H6',
+        'insertHorizontalRule': 'Infoga horisontell avdelare',
+        'linkDialog.show': 'Visa länkdialog'
+      },
+      history: {
+        undo: 'Ångra',
+        redo: 'Gör om'
+      },
+      specialChar: {
+        specialChar: 'SPECIELLA TECKEN',
+        select: 'Välj Specialtecken'
+      }
+    }
+  });
+})(jQuery);
